85191e0a9762fcfd2481df8d146c337a87fae4ec
QAsRevenge <hampus.e.molin@gmail.com>
2023-05-30 13:46:31 +0200

    removed api calls from server.js since were not using a backend

--- api/src/server.js
+++ api/src/server.js
@@ -4,14 +4,4 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-function handleRequest(req, res) {
-  res.json('Hello World');
-}
-
-app.get('/', handleRequest);
-
-app.get('/api/message', (req, res) => {
-  res.json('Hello from the API!');
-});
-
 app.listen(port, () => console.log('Server running on port ' + port));
