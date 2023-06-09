8c4e129b4809c3c064d6234f8c4c83533d8b7d99
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-06-09 12:03:26 +0200

    added a todo list

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -7,12 +7,55 @@ import { cn } from '@/util/cn'
 import dayjs from 'dayjs'
 import { useState } from 'react'
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 export function Pricing() {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
   const currentDate = dayjs()
   const [today, setToday] = useState(currentDate)
   const [selectedDate, setSelectedDate] = useState(currentDate)
+  const [todo, setTodo] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 100000)
+  }
+
+  const handleKeyUp = (key) => {
+    if (key === 'Enter' && newTodo) {
+      const randomNumber = getRandomNumber()
+      const newItem = {
+        id: `item-${randomNumber}`,
+        content: newTodo,
+      }
+
+      setTodo(todo.concat(newItem))
+      setNewTodo('')
+    }
+  }
+
+  const handleDelete = (id) => {
+    if (id > -1) {
+      setTodo(todo.filter((item) => item.id !== todo[id].id))
+    }
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+    return result
+  }
+
+  const handleOnDragEnd = (result) => {
+    const { destination, source } = result
+
+    if (!destination) {
+      return
+    }
+
+    const items = reorder(todo, source.index, destination.index)
+    setTodo(items)
+  }
 
   return (
     <section id="pricing" aria-label="Pricing" className="py-20 sm:py-32">
@@ -88,10 +131,55 @@ export function Pricing() {
             </div>
           </div>
           <div className="h-96 w-96 px-5">
-            <h1 className="font-semibold">
+            <h1 className="font-semibold shadow-md">
               Schedule for {selectedDate.toDate().toDateString()}
             </h1>
-            <p>No mettings for today.</p>
+            <p className="pt-1">Meetings for today.</p>
+            <div className="absolut inset-y-o pointer-events-none left-2 flex items-center pl-3"></div>
+            <input
+              type="text"
+              id="newTodo"
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+              onKeyUp={(e) => handleKeyUp(e.key)}
+              className="block w-full rounded-full border-2 bg-white p-2 pl-10 text-black placeholder-gray-500"
+              placeholder="Add new todo or meeting"
+            />
+                    <ul className="block w-full pt-6">
+                      {todo?.map((item, index) => {
+                        return (
+                          
+                                <li
+                                  key={item.id}
+                                  className="mt-2 w-full rounded-xl border-2 hover:border-blue-300"
+                                >
+                                  <input
+                                    id={index}
+                                    type="checkbox"
+                                    className="float-left m-3 block h-6 w-6"
+                                  />
+                                  <button
+                                    id={index}
+                                    onClick={() => handleDelete(index)}
+                                    className="float-right m-2.5 h-7 w-7 rounded-2xl bg-red-700 text-gray-200 shadow-md hover:scale-105 hover:bg-red-500"
+                                  >
+                                    x
+                                  </button>
+                                  <label
+                                    htmlFor={index}
+                                    className="block w-full p-3"
+                                  >
+                                    {item.content}
+                                  </label>
+                                </li>
+                              
+                            )}
+              )
+            }
+                      
+                    </ul>
+                    
+              
           </div>
         </div>
       </Container>
